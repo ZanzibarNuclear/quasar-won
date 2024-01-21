75df238da9130df5d423464d8977d611ad1148b2
Dave Mount <davemount@gmail.com>
2024-01-21 11:42:34 -0800

    Rough in feature card component

---
+++ src/components/FeatureCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-card class="my-card" flat bordered>
+    <q-card-section horizontal>
+      <q-card-section class="q-pt-xs">
+        <div class="text-overline">{{ category }}</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">{{ featureName }}</div>
+        <div class="text-caption text-grey">
+          {{ purpose }}
+        </div>
+      </q-card-section>
+
+      <q-card-section v-if="coverImage" class="col-5 flex flex-center">
+        <q-img class="rounded-borders" :src="coverImage" />
+      </q-card-section>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions align="center">
+      <q-btn color="primary" glossy stretch @click="goTo">
+        {{ callToAction }}
+      </q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const category = ref("Forum");
+const featureName = ref("What Say You");
+const coverImage = ref("https://cdn.quasar.dev/img/parallax2.jpg");
+const purpose = ref(
+  "Long-form discussions for sharing ideas about nuclear energy."
+);
+const callToAction = "Join In";
+const routeName = "what-say-you";
+const goTo = () => {
+  // navigate to routeName
+  alert(routeName);
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,101 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <feature-card />
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Forum</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">What Say You</div>
+          <div class="text-caption text-grey">
+            Long-form discussions for sharing ideas about nuclear energy.
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-5 flex flex-center">
+          <q-img
+            class="rounded-borders"
+            src="https://cdn.quasar.dev/img/parallax2.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="center">
+        <q-btn color="primary" glossy stretch> Join In </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Title</q-item-label>
+          <q-item-label caption> Subhead </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+
+        <q-separator vertical />
+
+        <q-card-section class="col-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-card-section horizontal>
+        <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax1.jpg" />
+
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat> 5:30PM </q-btn>
+        <q-btn flat> 7:00PM </q-btn>
+        <q-btn flat color="primary"> Reserve </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-card-section horizontal>
+        <q-img class="col" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+        <q-card-actions vertical class="justify-around">
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
+import FeatureCard from "src/components/FeatureCard.vue";
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
