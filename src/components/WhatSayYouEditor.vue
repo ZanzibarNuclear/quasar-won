361d08da3ce466044cacc5c7f3177456b9c93271
Dave Mount <davemount@gmail.com>
2024-01-24 21:53:23 -0800

    Experiment with what say you interface

---
+++ src/components/WhatSayYouEditor.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="q-pa-md" style="max-width: 450px">
+      <q-input
+        v-model="message"
+        filled
+        type="textarea"
+        @keydown.enter.exact.prevent="doIt"
+      />
+      <q-btn class="q-mt-md" push color="primary" label="Push" @click="doIt" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const message = ref('')
+const emit = defineEmits(['post'])
+
+const doIt = () => {
+  emit('post', message.value)
+  message.value = ''
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/WhatSayYouPage.vue
+++ src/pages/WhatSayYouPage.vue
@@ -1,13 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="q-ma-md">
     <div class="text-h2">"What Say You" Discussion Forum</div>
+    <what-say-you-editor @post="handlePost" />
+    <div class="q-ma-md">
+      <q-scroll-area style="height: 500px; max-width: 450px">
+        <div
+          v-for="(comment, index) in comments"
+          :key="index"
+          class="q-py-xs box"
+        >
+          {{ comment }}
+        </div>
+      </q-scroll-area>
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { ref } from 'vue'
+import WhatSayYouEditor from 'src/components/WhatSayYouEditor.vue'
 
-export default defineComponent({
-  name: "WhatSayYouPage",
-});
+const comments = ref([])
+
+const handlePost = (comment) => comments.value.push(comment)
 </script>
+
+<style scoped>
+.box {
+  border: 1px solid gray;
+  margin: 0.5rem;
+  padding: 0.25rem;
+  flex-direction: column;
+}
+</style>
